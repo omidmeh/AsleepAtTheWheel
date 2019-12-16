59eda62d31a5187694888c8cf35c0afa0ec6d55a
Melbschneider <mbschneider@utexas.edu>
2019-12-15 18:06:30 -0600

    Latest facial feature LSTM file

--- clean_face.py
+++ clean_face.py
@@ -6,128 +6,88 @@ Created on Thu Dec 12 15:25:15 2019
 """
 import pandas as pd
 import numpy as np 
-from math import sqrt
-from numpy import concatenate
 from matplotlib import pyplot
-from pandas import read_csv
-from pandas import DataFrame
-from pandas import concat
 from sklearn.preprocessing import MinMaxScaler
-from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import mean_squared_error
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.layers import LSTM
 from keras.utils import to_categorical
-from numpy import split
-from numpy import array
 import sys 
-from sklearn.model_selection import train_test_split
 sys.path.append("C:\\Users\\melbs\\OneDrive\\Desktop\\DSL")
 from omid import *
-
-# %% convert series to supervised learning
-def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
-	n_vars = 1 if type(data) is list else data.shape[1]
-	df = DataFrame(data)
-	cols, names = list(), list()
-	# input sequence (t-n, ... t-1)
-	for i in range(n_in, 0, -1):
-		cols.append(df.shift(i))
-		names += [('var%d(t-%d)' % (j+1, i)) for j in range(n_vars)]
-	# forecast sequence (t, t+1, ... t+n)
-	for i in range(0, n_out):
-		cols.append(df.shift(-i))
-		if i == 0:
-			names += [('var%d(t)' % (j+1)) for j in range(n_vars)]
-		else:
-			names += [('var%d(t+%d)' % (j+1, i)) for j in range(n_vars)]
-	# put it all together
-	agg = concat(cols, axis=1)
-	agg.columns = names
-	# drop rows with NaN values
-	if dropnan:
-		agg.dropna(inplace=True)
-	return agg
 
 # %%
 # Load and prepare dataset
 ir_val = '100ms'
+n_steps = 20
+#n_size = int(4000)
 # Load and prepare dataset
-df03 = get_table(34,10, resample_interval= ir_val)
-df04 = get_table(37,0, resample_interval= ir_val)
-df05 = get_table(37,10, resample_interval= ir_val)
-df06 = get_table(49,0, resample_interval= ir_val)
-df09 = get_table(27,0, resample_interval= ir_val)
-df10 = get_table(27,10, resample_interval= ir_val)
-df11 = get_table(18,0, resample_interval= ir_val)
-df12 = get_table(50,10, resample_interval= ir_val)
-df13 = get_table(26,0, resample_interval= ir_val)
-df14 = get_table(26,10, resample_interval= ir_val)
-df01 = get_table(60,0, resample_interval= ir_val)
-df02 = get_table(31,10, resample_interval= ir_val)
-df03 = get_table(60,0, resample_interval= ir_val)
-df04 = get_table(45,10, resample_interval= ir_val)
-df05 = get_table(51,0, resample_interval= ir_val)
-df06 = get_table(51,10, resample_interval= ir_val)
-df07 = get_table(31,0, resample_interval= ir_val)
-df08 = get_table(45,10, resample_interval= ir_val)
+df01= get_table(18,0, resample_interval= ir_val)#[:n_size]
+df02 = get_table(26,0, resample_interval= ir_val)#[:n_size]
+df03 = get_table(26,10, resample_interval= ir_val)#[:n_size]
+df04 = get_table(27,0, resample_interval= ir_val)#[:n_size]
+df05 = get_table(27,10, resample_interval= ir_val)#[:n_size]
+df06 = get_table(34,10, resample_interval= ir_val)#[:n_size]
+df07 = get_table(37,0, resample_interval= ir_val)#[:n_size]
+df08 = get_table(37,10, resample_interval= ir_val)#[:n_size]
+df09 = get_table(45,10, resample_interval= ir_val)#[:n_size]
+df10 = get_table(49,0, resample_interval= ir_val)#[:n_size]
+df11 = get_table(50,10, resample_interval= ir_val)#[:n_size]
+df12 = get_table(51,0, resample_interval= ir_val)#[:n_size]
+df13 = get_table(60,0, resample_interval= ir_val)#[:n_size]
+df14 = get_table(60,10, resample_interval= ir_val)#[:n_size]
+df15 = get_table(31,10, resample_interval= ir_val)#[:n_size]
+df16 = get_table(31,0, resample_interval= ir_val)#[:n_size]
 #%%
-dfs = [df01,df02,df03,df04,df05,df06,df07,df08]
+dfs = [df01,df02,df03,df04,df05,df06,df07,df08,df09,df10,df11,df12,df13,df14,df15,df16]
 #np.random.shuffle(dfs)
 dataset = pd.concat(dfs,axis=0)
 #scaled = make_scale(dataset)
 # %%
-def dist(mx, my ,nx, ny):
-    return np.sqrt(np.square(mx-nx) + np.square(my-ny))
+#def prepare_data(dataset):
 
-def mid(x1, x2):
-    return (x1+x2)/2
-
-def ratio_6(table, t1,t2,b1,b2,l,r):
-    x1_m= mid(table[f'px_{t1}'], table[f'px_{t2}'])
-    y1_m = mid(table[f'py_{t1}'], table[f'py_{t2}'])
-    x2_m = mid(table[f'px_{b1}'], table[f'px_{b2}'])
-    y2_m = mid(table[f'py_{b1}'], table[f'py_{b2}'])
-
-    return dist(x1_m,y1_m,x2_m,y2_m) / dist(table[f'px_{l}'], table[f'py_{l}'], table[f'px_{r}'], table[f'py_{r}'])
-
-
-dataset['eye_l_ratio'] = ratio_6(dataset,38,39,41,42,37,40)
+    # add eye and mouth ratios 
+dataset['eye_l_ratio'] = ratio_6(dataset,38,39,41,42,37,40) 
 dataset['eye_r_ratio'] = ratio_6(dataset,44,45,47,48,43,46)
+#dataset['mouth_ratio'] = ratio_4(dataset,52,58,49,55)
 dataset['mouth_ratio'] = ratio_6(dataset,44,45,47,48,43,46)
-# %% 
 # drop face columns 
-dataset.drop(['face_x', 'face_y', 'face_w', 'face_h','time','participant'], axis=1,inplace=True)
-#dataset = make_xy(dataset)
-# %%
-# specify number of timesteps 
-n_steps = 20
-n_features = dataset.shape[1]-1
-# frame as supervised learning
+dataset.drop(['face_x', 'face_y', 'face_w', 'face_h','time'], axis=1,inplace=True)
+
 values = dataset
 values = values.astype('float32')
-values.drop(['mood'],axis=1,inplace=True)
-# %%
+values.drop(['mood','participant'],axis=1,inplace=True)
+# Define x 
 X = values
-#reframed = series_to_supervised(X, n_steps, 1)
-#print(reframed.shape)    
-#X = array(reframed)
-# %% normalize features
+#X.drop(['participant'])
+# normalize features
 scaler = MinMaxScaler(feature_range=(0, 1))
 X = scaler.fit_transform(X)
 # define y 
 y = dataset.mood[0:X.shape[0]].values
 y[y!=0]=1
 y = to_categorical(y)
+    #return dataset, X, y 
+#dataset, X, y = prepare_data(dataset)
 # %%
 # split into train and test sets
-train_size = int((dataset.shape[0]*(3/4)))
+train_size = int((dataset.shape[0]*(14/16)))
 X_train = X[0:train_size] 
 X_test = X[train_size:X.shape[0]]
 y_train = y[0:train_size] 
 y_test = y[train_size:X.shape[0]]
 # %%
+# Reshape input to 3D
+#def reshape_3d(X,y,n_steps):
+#    resize = int(X.shape[0]/n_steps)
+#    X.reshape(resize,n_steps,X.shape[1])
+#    y[::n_steps]
+#    return X,y
+#    
+#X_train,y_train = reshape_3d(X_train,y_train,n_steps)
+#X_test,y_test = reshape_3d(X_test,y_test,n_steps)
+#
+#print(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
 # Reshape input to 3D 
 train_resize = int(X_train.shape[0]/n_steps)
 test_resize = int(X_test.shape[0]/n_steps)
@@ -138,12 +98,8 @@ X_test = X_test.reshape(test_resize,n_steps,X.shape[1])
 y_test = y_test[::n_steps]
 print(X_train.shape, y_train.shape, X_test.shape, y_test.shape)
 #%% 
-# design network 
+# design and train LSTM  network 
 from keras.layers import Dropout
-
-# tune learning parameters 
-# tune optimizers  (adam)
-# tune lstm input 
 import timeit
 
 start = timeit.default_timer()
@@ -183,19 +139,33 @@ pyplot.show()
 
 #%%
 y_pred = model.predict(X_test)
+probas = model.predict_proba(X_test)
+   # Compute ROC curve and area the curve
+#fpr, tpr, thresholds = roc_curve(y[test], probas_[:, 1])
+#tprs.append(interp(mean_fpr, fpr, tpr))
+#tprs[-1][0] = 0.0
+#roc_auc = auc(fpr, tpr)
 # convert to original format 
 #y_pred = np.argmax(y_pred,1)
 #y_true = np.argmax(y_test,1)
 
 # %%
 pyplot.plot(y_pred, label='y_pred')
-pyplot.plot(y_true, label='y_true')
+pyplot.plot(y_test, label='y_true')
 pyplot.legend()
 pyplot.show()
 
 # %%
 y_pred = pd.DataFrame(y_pred)
-prediction = y_pred.to_csv('prediction.csv')
+y_true = pd.DataFrame(y_test)
+#%% 
+test_set = dataset[['participant','mood','eye_l_ratio','eye_r_ratio','mouth_ratio']]
+test_set = test_set[train_size:X.shape[0]]
+predictions = test_set[::20]
+predictions['alert'] = y_pred[[0]].values
+predictions['drowsy'] = y_pred[[1]].values
+#predictions.to_csv('prediction.csv')
+
 # %% 
 #for j in y_pred:
 #	if j[0]>j[1]:
